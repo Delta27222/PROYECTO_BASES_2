fcffb9738af349af537bfa5b7ec58d60f53f6592
Javier <javiereduardorh@gmail.com>
2022-11-13 15:57:19 -0400

    Fix Reporte 1 & tablas

--- Codigo_importante/Reportes/Reporte1.sql
+++ Codigo_importante/Reportes/Reporte1.sql
@@ -2,23 +2,25 @@ create or replace procedure reporte_1(cur in out sys_refcursor, fecha_ini DATE, 
 is 
 begin
     open cur for
-        select avg.sucursal, p.nombre platos, c.tipo, count(pc.id_plato) cant_veces_sol,
+        select s.direccion, p.nombre platos, c.tipo, count(pc.id_plato) cant_veces_sol,
         concat(round((count(pc.id_plato)*100)/avg.porcentaje),'%') porcentaje, p.precio precio_unitario
         from plato_consumido pc
         join consumo c
         on c.id = pc.id_consumo
         join (
-            Select pc.sucursal, count(pc.id_plato) porcentaje
+            Select c.ID_sucursal sucursal, count(pc.id_plato) porcentaje
             from plato_consumido pc
             join consumo c
             on c.id = pc.id_consumo
-            group by pc.sucursal
+            group by c.ID_sucursal
         ) avg
-        on pc.sucursal = avg.sucursal
+        on c.id_sucursal = avg.sucursal
         join plato p
         on p.id = pc.id_plato
+        JOIN SUCURSAL S
+        ON s.id = c.id_sucursal
         where c.fecha_consumo.fecha_inicio BETWEEN fecha_ini and fecha_fin
-        group by p.nombre, avg.porcentaje, avg.sucursal, c.tipo, p.precio
+        group by p.nombre, avg.porcentaje, s.direccion, c.tipo, p.precio
         order by avg.sucursal, p.nombre, c.tipo;
 end;
 
