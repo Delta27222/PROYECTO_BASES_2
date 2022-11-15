cf2c9bd22bba5dbb0d0d67b5dae2fc3096223b75
Delta27222 <angelgabriel.hernandez@gmail.com>
2022-11-15 11:51:29 -0800

    Codigo para eliminar y crear nuevas secuencias

---
+++ Eliminar_Crear_Secuencias.sql
@@ -0,0 +1,157 @@
+
+ drop sequence SEQ_SUCURSAL;
+ drop sequence SEQ_PERSONA;
+ drop sequence SEQ_INVENTARIO;
+ drop sequence SEQ_GRUPO;
+ drop sequence SEQ_EVENTO;
+ drop sequence SEQ_EMPLEADO;
+ drop sequence SEQ_PUESTO;
+ drop sequence SEQ_ROL;
+ drop sequence SEQ_RESERVA;
+ drop sequence SEQ_MESA_RESERVADA;
+ drop sequence SEQ_MESA;
+ drop sequence SEQ_CONTABILIDAD;
+ drop sequence SEQ_RESTAURANTE;
+ drop sequence SEQ_CONSUMO;
+ drop sequence SEQ_RECETA;
+ drop sequence SEQ_PLATO;
+ drop sequence SEQ_PLATO_CONSUMIDO;
+ drop sequence SEQ_PLATO_DIA;
+ drop sequence SEQ_PROMOCION;
+drop sequence SEQ_COMPRA;
+drop sequence SEQ_PAGO;
+drop sequence SEQ_PRODUCTO;
+drop sequence SEQ_ENCUESTA;
+
+
+CREATE SEQUENCE SEQ_RESTAURANTE --nombre de la secuencia
+START WITH 1 --la secuencia empieza por 1
+INCREMENT BY 1 --se incrementa de uno en uno
+NOMAXVALUE; --no tiene valor maximo
+
+CREATE SEQUENCE SEQ_SUCURSAL --nombre de la secuencia
+START WITH 1 --la secuencia empieza por 1
+INCREMENT BY 1 --se incrementa de uno en uno
+NOMAXVALUE; --no ti
+
+CREATE SEQUENCE SEQ_PRODUCTO --nombre de la secuencia
+START WITH 1 --la secuencia empieza por 1
+INCREMENT BY 1 --se incrementa de uno en uno
+NOMAXVALUE; --no tiene valor maximo
+
+CREATE SEQUENCE SEQ_INVENTARIO --nombre de la secuencia
+START WITH 1 --la secuencia empieza por 1
+INCREMENT BY 1 --se incrementa de uno en uno
+NOMAXVALUE; --no tiene valor maximo
+
+
+CREATE SEQUENCE SEQ_GRUPO --nombre de la secuencia
+START WITH 1 --la secuencia empieza por 1
+INCREMENT BY 1 --se incrementa de uno en uno
+NOMAXVALUE; --no tiene valor maximo
+
+CREATE SEQUENCE SEQ_EVENTO --nombre de la secuencia
+START WITH 1 --la secuencia empieza por 1
+INCREMENT BY 1 --se incrementa de uno en uno
+NOMAXVALUE; --no tiene valor maximo
+
+
+
+CREATE SEQUENCE SEQ_PERSONA --nombre de la secuencia
+START WITH 1 --la secuencia empieza por 1
+INCREMENT BY 1 --se incrementa de uno en uno
+NOMAXVALUE; --no tiene valor maximo
+
+CREATE SEQUENCE SEQ_EMPLEADO --nombre de la secuencia
+START WITH 1 --la secuencia empieza por 1
+INCREMENT BY 1 --se incrementa de uno en uno
+NOMAXVALUE; --no tiene valor maximo
+
+CREATE SEQUENCE SEQ_PLATO --nombre de la secuencia
+START WITH 1 --la secuencia empieza por 1
+INCREMENT BY 1 --se incrementa de uno en uno
+NOMAXVALUE; --no tiene valor maximo
+
+
+CREATE SEQUENCE SEQ_ENCUESTA --nombre de la secuencia
+START WITH 1 --la secuencia empieza por 1
+INCREMENT BY 1 --se incrementa de uno en uno
+NOMAXVALUE; --no tiene valor maximo
+
+CREATE SEQUENCE SEQ_ROL --nombre de la secuencia
+START WITH 1 --la secuencia empieza por 1
+INCREMENT BY 1 --se incrementa de uno en uno
+NOMAXVALUE; --no tiene valor maximo
+
+
+CREATE SEQUENCE SEQ_PUESTO --nombre de la secuencia
+START WITH 1 --la secuencia empieza por 1
+INCREMENT BY 1 --se incrementa de uno en uno
+NOMAXVALUE; --no tiene valor maximo
+
+CREATE SEQUENCE SEQ_MESA --nombre de la secuencia
+START WITH 1 --la secuencia empieza por 1
+INCREMENT BY 1 --se incrementa de uno en uno
+NOMAXVALUE; --no tiene valor maximo
+
+CREATE SEQUENCE SEQ_RESERVA --nombre de la secuencia
+START WITH 1 --la secuencia empieza por 1
+INCREMENT BY 1 --se incrementa de uno en uno
+NOMAXVALUE; --no tiene valor maximo
+
+
+CREATE SEQUENCE SEQ_MESA_RESERVADA --nombre de la secuencia
+START WITH 1 --la secuencia empieza por 1
+INCREMENT BY 1 --se incrementa de uno en uno
+NOMAXVALUE; --no tiene valor maximo
+
+
+CREATE SEQUENCE SEQ_CONTABILIDAD --nombre de la secuencia
+START WITH 1 --la secuencia empieza por 1
+INCREMENT BY 1 --se incrementa de uno en uno
+NOMAXVALUE; --no tiene valor maximo
+
+
+CREATE SEQUENCE SEQ_COMPRA --nombre de la secuencia
+START WITH 1 --la secuencia empieza por 1
+INCREMENT BY 1 --se incrementa de uno en uno
+NOMAXVALUE; --no tiene valor maximo
+
+
+CREATE SEQUENCE SEQ_PAGO --nombre de la secuencia
+START WITH 1 --la secuencia empieza por 1
+INCREMENT BY 1 --se incrementa de uno en uno
+NOMAXVALUE; --no tiene valor maximo
+
+
+CREATE SEQUENCE SEQ_CONSUMO  --nombre de la secuencia
+START WITH 1 --la secuencia empieza por 1
+INCREMENT BY 1 --se incrementa de uno en uno
+NOMAXVALUE; --no tiene valor maximo
+
+CREATE SEQUENCE SEQ_RECETA --nombre de la secuencia
+START WITH 1 --la secuencia empieza por 1
+INCREMENT BY 1 --se incrementa de uno en uno
+NOMAXVALUE; --no tiene valor maximo
+
+
+CREATE SEQUENCE SEQ_PLATO_CONSUMIDO --nombre de la secuencia
+START WITH 1 --la secuencia empieza por 1
+INCREMENT BY 1 --se incrementa de uno en uno
+NOMAXVALUE; --no tiene valor maximo
+
+CREATE SEQUENCE SEQ_PLATO_DIA --nombre de la secuencia
+START WITH 1 --la secuencia empieza por 1
+INCREMENT BY 1 --se incrementa de uno en uno
+NOMAXVALUE; --no tiene valor maximo
+
+CREATE SEQUENCE SEQ_PROMOCION --nombre de la secuencia
+START WITH 1 --la secuencia empieza por 1
+INCREMENT BY 1 --se incrementa de uno en uno
+NOMAXVALUE; --no tiene valor maximo
+
+
+
+
+
+
