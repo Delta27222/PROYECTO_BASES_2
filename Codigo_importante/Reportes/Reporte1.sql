e5ad2da19cc55f72f38fec5836ceacd28a2e8602
Javier <javiereduardorh@gmail.com>
2022-11-17 22:51:31 -0400

    Chamo yo vuelvo hacer un push de este reporte 1, te lo juro me cambio a filosofia

--- Codigo_importante/Reportes/Reporte1.sql
+++ Codigo_importante/Reportes/Reporte1.sql
@@ -1,21 +1,31 @@
-create or replace procedure reporte_1(cur in out sys_refcursor, fecha_ini in out DATE, fecha_fin in out DATE, tipo_consumo in out String, tipo_plato in out String)
+create or replace procedure reporte_1(cur in out sys_refcursor, fecha_ini in DATE, fecha_fin in DATE, tipo_consumo in String, tipo_plato in  String)
 is 
+Begin
+declare
+    vr_fecha_ini DATE;
+    vr_fecha_fin DATE;
+    vr_tipo_consumo consumo.tipo%type;
+    vr_tipo_plato plato.categoria%type;
 begin
 
     if (tipo_consumo is null) then
-        tipo_consumo:= '%';
+        vr_tipo_consumo:= '%';
+    else vr_tipo_consumo:= tipo_consumo;
     end if;
     
     if (tipo_plato is null) then
-        tipo_plato:='%';
+        vr_tipo_plato:='%';
+    else vr_tipo_plato:=tipo_plato;    
     end if;
     
     if (fecha_ini is null) then
-        select min(c.fecha_consumo.fecha_inicio) into fecha_ini from consumo c;
+        select min(c.fecha_consumo.fecha_inicio) into vr_fecha_ini from consumo c;
+    else vr_fecha_ini:= to_date(fecha_ini);    
     end if;
     
     if (fecha_fin is null) then
-        select max(c.fecha_consumo.fecha_inicio) into fecha_fin from consumo c;
+        select max(c.fecha_consumo.fecha_inicio) into vr_fecha_fin from consumo c;
+    else vr_fecha_fin:= to_date(fecha_fin);
     end if;
     
         open cur for
@@ -31,9 +41,9 @@ begin
                 on pc.id_consumo = c.id
                 join plato p
                 on p.id = pc.id_plato
-                where c.fecha_consumo.fecha_inicio between fecha_ini and fecha_fin
-                and c.tipo like tipo_consumo
-                and p.categoria like tipo_plato
+                where c.fecha_consumo.fecha_inicio between vr_fecha_ini and vr_fecha_fin
+                and c.tipo like vr_tipo_consumo
+                and p.categoria like vr_tipo_plato
                 group by s.direccion
             ) t
             on t.direccion = s.direccion
@@ -46,16 +56,17 @@ begin
                 on pc.id_consumo = c.id
                 join plato p
                 on p.id = pc.id_plato
-                where c.fecha_consumo.fecha_inicio between fecha_ini and fecha_fin
-                and c.tipo like tipo_consumo
-                and p.categoria like tipo_plato
+                where c.fecha_consumo.fecha_inicio between vr_fecha_ini and vr_fecha_fin
+                and c.tipo like vr_tipo_consumo
+                and p.categoria like vr_tipo_plato
                 group by s.direccion, p.nombre, c.tipo
             ) d
             on d.direccion = s.direccion
             join plato p
             on p.nombre = d.plato_nombre
-            order by s.direccion;
+            order by t.f_ini,s.direccion, d.tipo,p.categoria;
     
+end;
 end;
 
 
