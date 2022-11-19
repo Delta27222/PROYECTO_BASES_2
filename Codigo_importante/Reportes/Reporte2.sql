f114bafbbd0d231527939618791583e4f892376f
Delta27222 <angelgabriel.hernandez@gmail.com>
2022-11-19 13:11:48 -0800

    Le hice un cambio al reportes2, para poder comprar lo de las fechas

--- Codigo_importante/Reportes/Reporte2.sql
+++ Codigo_importante/Reportes/Reporte2.sql
@@ -1,16 +1,16 @@
 create or replace NONEDITIONABLE procedure reporte_2(cur in out sys_refcursor, fecha_var DATE)
 is
 begin
-   
-    if fecha_var is not null then
+
+    if (fecha_var is not null )then
         open cur for
-            select pd.fecha_pla_dia.FECHA_INICIO fecha, p.categoria, p.nombre, p.foto, p.descripcion, p.precio.MONTO || p.precio.UNIDAD_MONTO precio
+            select pd.fecha_pla_dia.FECHA_INICIO fecha, p.categoria, p.nombre, p.foto, p.descripcion, p.precio.MONTO || p.precio.UNIDAD_MONTO  precio
             from plato p
             join plato_dia pd
             on p.id = pd.id_plato
-            where pd.fecha_pla_dia.FECHA_INICIO = fecha_var; 
+            where to_date(pd.fecha_pla_dia.FECHA_INICIO) = to_date(fecha_var); 
     else         
---        open cur for
+        open cur for
             select pd.fecha_pla_dia.FECHA_INICIO fecha, p.categoria, p.nombre, p.foto, p.descripcion, p.precio.MONTO || p.precio.UNIDAD_MONTO precio
             from plato p
             join plato_dia pd
