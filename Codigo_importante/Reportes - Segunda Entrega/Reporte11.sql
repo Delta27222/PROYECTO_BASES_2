12a0acb84da72d77aa24e4acd65ece91096c4272
Delta27222 <angelgabriel.hernandez@gmail.com>
2023-01-04 13:04:42 -0800

    Creacion de la carpeta de los reportes de la segunda entrega, junto con los resportes 7 y 11. El reporte 7 aun tiene un problema con la columna de cantidad PREGUNTAR A JAVIER

---
+++ Codigo_importante/Reportes - Segunda Entrega/Reporte11.sql
@@ -0,0 +1,36 @@
+create or replace NONEDITIONABLE procedure ZZ_reporte_11(cur in out sys_refcursor,destino String, tipo_comida String)
+is 
+Begin
+Declare 
+    f_ini DATE;
+    f_fin DATE;
+    vr_ubicacion_delivery consumo.ubicacion%type;
+    vr_tipo_comida plato.categoria%type;
+    Begin
+
+        if (destino is null) then
+            vr_ubicacion_delivery:= '%';
+        else vr_ubicacion_delivery:= lower(translate(destino,'áéíóúÁÉÍÓÚ','aeiouAEIOU'));
+        end if;
+
+        if (tipo_comida is null) then
+            vr_tipo_comida:= '%';
+        else vr_tipo_comida:= lower(translate(tipo_comida,'áéíóúÁÉÍÓÚ','aeiouAEIOU'));
+        end if;
+ open cur for
+        select suc.direccion Sucursal, x.categoria Tipo_comida,x.nombre Nombre, x.foto Foto, 'Delivery' Tipo_consumo, per.nombre Nombre_cliente, per.apellido Apellido_cliente, per.telefono Telefono, con.ubicacion Direccion_entrega
+        from consumo con
+        join sucursal suc
+        on con.id_sucursal = suc.id
+        join persona per
+        on con.id_cliente = per.id
+        join(
+            select * from plato pla
+            join plato_consumido plaC
+            on pla.id = plaC.id_plato
+        ) x
+        on con.id = x.id_consumo
+        where lower(translate(con.ubicacion,'áéíóúÁÉÍÓÚ','aeiouAEIOU')) like vr_ubicacion_delivery
+        and lower(translate(x.categoria,'áéíóúÁÉÍÓÚ','aeiouAEIOU')) like vr_tipo_comida;
+    end;
+end;
---
+++ Codigo_importante/Reportes - Segunda Entrega/Reporte7.sql
@@ -0,0 +1,34 @@
+create or replace NONEDITIONABLE procedure Z_reporte_7(cur in out sys_refcursor,nombre_suc String, fecha_inicio_var date, fecha_fin_var date)
+is 
+Begin
+Declare 
+    f_ini DATE;
+    f_fin DATE;
+    vr_nombre_suc sucursal.direccion%type;
+    Begin
+
+        if (fecha_inicio_var is null) then
+            select min(inv.fecha_inventario.fecha_inicio) into f_ini from inventario inv;
+        else f_ini:= to_date(fecha_inicio_var);    
+        end if;
+
+        if (fecha_fin_var is null) then
+            select max(inv.fecha_inventario.fecha_inicio) into f_fin from inventario inv;
+        else f_fin:= to_date(fecha_fin_var);    
+        end if;
+
+        if (nombre_suc is null) then
+            vr_nombre_suc:= '%';
+        else vr_nombre_suc:= lower(translate(nombre_suc,'áéíóúÁÉÍÓÚ','aeiouAEIOU'));
+        end if;
+    open cur for
+        select suc.direccion sucursal, inv.fecha_inventario.fecha_inicio fecha_inventario, pro.nombre producto, pro.foto foto, '3' as cantidad
+        from inventario inv
+        join sucursal suc
+        on (inv.id_sucursal = suc.id)
+        join producto pro
+        on inv.id_producto = pro.id
+        where inv.fecha_inventario.fecha_inicio between f_ini and f_fin
+        and lower(translate(suc.direccion,'áéíóúÁÉÍÓÚ','aeiouAEIOU')) like vr_nombre_suc;
+    end;
+end;
