e37e82b59c7a37ad33b6840a7558252e9906aed3
Delta27222 <angelgabriel.hernandez@gmail.com>
2023-01-12 12:34:17 -0800

    Agragando el reporte 9, ya funcionando en todos lados, con imagenes incluidas

--- Codigo_importante/Reportes - Segunda Entrega/Reporte9.sql
+++ Codigo_importante/Reportes - Segunda Entrega/Reporte9.sql
@@ -19,62 +19,74 @@ end if;
 
 
 open cur for
-    select s.direccion sucursal, b.mes, (round(((b.promotores*10)/a.cant_respuestas),2)) nivel, to_char(b.observaciones) observaciones
-        from sucursal s
-        join (
-            select id_sucursal, p.mes, count(valoracion) cant_respuestas
-            from encuesta e
-            join (
-                select e.id,
-                CONCAT(CONCAT(decode ((select extract (MONTH FROM e.fecha_encuesta.fecha_inicio) FROM DUAL),
-                01,'ENERO',
-                02,'FEBRERO',
-                03,'MARZO',
-                04,'ABRIL',
-                05,'MAYO',
-                06,'JUNIO',
-                07,'JULIO',
-                08,'AGOSTO',
-                09,'SEPTIEMBRE',
-                10,'OCTUBRE',
-                11,'NOVIEMBRE',
-                12,'DICIEMBRE'),' '), (select extract (YEAR FROM e.fecha_encuesta.fecha_inicio) FROM DUAL)) MES
-                from encuesta e
-                where extract(month from e.fecha_encuesta.fecha_inicio) between var_m_ini and var_m_fin
-            ) p
-            on p.id = e.id
-            group by id_sucursal, p.mes
-            order by id_sucursal, p.mes desc
-        ) a 
-        on a.id_sucursal = s.id
-        join (
-            select id_sucursal, p.mes, sum(valoracion) promotores, listagg(concat('o.- ',e.observacion),chr(13)) 
-            WITHIN GROUP (ORDER BY e.ID_sucursal) as observaciones
-            from encuesta e
-            join (
-                select e.id, 
-                CONCAT(CONCAT(decode ((select extract (MONTH FROM e.fecha_encuesta.fecha_inicio) FROM DUAL),
-                01,'ENERO',
-                02,'FEBRERO',
-                03,'MARZO',
-                04,'ABRIL',
-                05,'MAYO',
-                06,'JUNIO',
-                07,'JULIO',
-                08,'AGOSTO',
-                09,'SEPTIEMBRE',
-                10,'OCTUBRE',
-                11,'NOVIEMBRE',
-                12,'DICIEMBRE'),' '), (select extract (YEAR FROM e.fecha_encuesta.fecha_inicio) FROM DUAL)) MES
-                from encuesta e
-                where extract(month from e.fecha_encuesta.fecha_inicio) between var_m_ini and var_m_fin
-            ) p
-            on p.id = e.id
-            group by id_sucursal, p.mes
-            order by id_sucursal, p.mes desc
-        )b
-        on b.id_sucursal = s.id
-        and a.mes = b.mes
-        where s.direccion like var_n_sucursal;
+            select x.sucursal,x.mes, x.nivel , x.observaciones,
+            case 
+            when (x.nivel >= 90) and (x.nivel <= 100) then (select face_img from face where id=1)
+            when (x.nivel >= 70) and (x.nivel <= 89) then (select face_img from face where id=2)
+            when (x.nivel >= 50) and (x.nivel <= 69) then (select face_img from face where id=3)
+            when (x.nivel <= 49) then (select face_img from face where id=4)
+            end img
+            from face s
+            join
+            (
+            select s.direccion sucursal, b.mes mes, (round(((b.promotores*10)/a.cant_respuestas),2)) nivel, to_char(b.observaciones) observaciones
+                    from sucursal s
+                    join (
+                        select id_sucursal, p.mes, count(valoracion) cant_respuestas
+                        from encuesta e
+                        join (
+                            select e.id,
+                            CONCAT(CONCAT(decode ((select extract (MONTH FROM e.fecha_encuesta.fecha_inicio) FROM DUAL),
+                            01,'ENERO',
+                            02,'FEBRERO',
+                            03,'MARZO',
+                            04,'ABRIL',
+                            05,'MAYO',
+                            06,'JUNIO',
+                            07,'JULIO',
+                            08,'AGOSTO',
+                            09,'SEPTIEMBRE',
+                            10,'OCTUBRE',
+                            11,'NOVIEMBRE',
+                            12,'DICIEMBRE'),' '), (select extract (YEAR FROM e.fecha_encuesta.fecha_inicio) FROM DUAL)) MES
+                            from encuesta e
+                            where extract(month from e.fecha_encuesta.fecha_inicio) between var_m_ini and var_m_fin
+                        ) p
+                        on p.id = e.id
+                        group by id_sucursal, p.mes
+                        order by id_sucursal, p.mes desc
+                    ) a 
+                    on a.id_sucursal = s.id
+                    join (
+                        select id_sucursal, p.mes, sum(valoracion) promotores, listagg(concat('o.- ',e.observacion),chr(13)) 
+                        WITHIN GROUP (ORDER BY e.ID_sucursal) as observaciones
+                        from encuesta e
+                        join (
+                            select e.id, 
+                            CONCAT(CONCAT(decode ((select extract (MONTH FROM e.fecha_encuesta.fecha_inicio) FROM DUAL),
+                            01,'ENERO',
+                            02,'FEBRERO',
+                            03,'MARZO',
+                            04,'ABRIL',
+                            05,'MAYO',
+                            06,'JUNIO',
+                            07,'JULIO',
+                            08,'AGOSTO',
+                            09,'SEPTIEMBRE',
+                            10,'OCTUBRE',
+                            11,'NOVIEMBRE',
+                            12,'DICIEMBRE'),' '), (select extract (YEAR FROM e.fecha_encuesta.fecha_inicio) FROM DUAL)) MES
+                            from encuesta e
+                            where extract(month from e.fecha_encuesta.fecha_inicio) between var_m_ini and var_m_fin
+                        ) p
+                        on p.id = e.id
+                        group by id_sucursal, p.mes
+                        order by id_sucursal, p.mes desc
+                    )b
+                    on b.id_sucursal = s.id
+                    and a.mes = b.mes
+                    where s.direccion like var_n_sucursal
+            )x
+            on  id = 2;     
 end;    
 end;
